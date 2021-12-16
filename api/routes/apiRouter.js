9c20bb31094d9d59ce9144cbf5ace7d98daedcf7
CPD <mundim.github@gmail.com>
2021-12-16 13:35:34 -0300

    Terceira versao

--- api/routes/apiRouter.js
+++ api/routes/apiRouter.js
@@ -1,9 +1,17 @@
 const express = require ('express')
 let apiRouter = express.Router()
 
+const knex = require('knex')({
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+})
+
 let endpoint = '/'
 apiRouter.get(endpoint + 'produtos', (req, res) => {
-    res.status(200).json ({id:1, descricao: 'produto 1'})
+    knex
+    .select('*')
+    .from('produto')
+    .then(produtos => res.status(200).json(produtos))
 })
 
 module.exports = apiRouter
